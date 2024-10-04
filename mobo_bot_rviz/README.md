30f5f8df15cc95ea705812a36a4c24207ceb2086
samuko-things <samuel.c.agba@gmail.com>
2024-10-04 13:08:21 +0100

    add rviz package

---
+++ mobo_bot_rviz/README.md
@@ -0,0 +1,4 @@
+### mobo_bot_rviz
+RVIZ visualizations of the **mobo_bot** differential drive robot. The visualizations are used by both the robot simulation and the real (actual) robot. 
+
+#
---
+++ mobo_bot_rviz/launch/model.launch.py
@@ -0,0 +1,48 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+
+
+
+def generate_launch_description():
+    # delare any path variable
+    # description_pkg_path = os.path.join(get_package_share_directory('mobo_bot_description'))
+    rviz_pkg_path = get_package_share_directory('mobo_bot_rviz')
+    
+    rviz_config_file = os.path.join(rviz_pkg_path,'config','model.rviz')
+
+
+    # create needed nodes or launch files
+    
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+    
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen'
+    )
+
+
+     # Create the launch description and populate
+    ld = LaunchDescription()
+    
+
+    # Add the nodes to the launch description
+    ld.add_action(rviz_node)
+    ld.add_action(joint_state_publisher_gui_node)
+
+    
+    return ld      # return (i.e send) the launch description for excecution
+
