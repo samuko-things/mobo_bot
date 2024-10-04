f69d544602ef85b7d6685aaee14ba7d2045c11a7
samuko-things <samuel.c.agba@gmail.com>
2024-10-04 16:58:29 +0100

    update robot description

--- mobo_bot_description/launch/rsp.launch.py
+++ mobo_bot_description/launch/rsp.launch.py
@@ -18,20 +18,32 @@ def generate_launch_description():
     xacro_file = os.path.join(description_pkg_path,'urdf','robot_urdf.xacro')
 
     # Check if we're told to use sim time
+    use_simulation = LaunchConfiguration('use_simulation')
     use_sim_time = LaunchConfiguration('use_sim_time')
     
-     # declare launch arguments
+    # declare launch arguments
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='true',
+        default_value='false',
         description='Use sim time if true'
     )
 
+    declare_use_simulation_cmd = DeclareLaunchArgument(
+        'use_simulation',
+        default_value='false',
+        description='Use simulation if true'
+    )
 
     # Create a robot_state_publisher node
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
-    params = {'robot_description': doc.toxml(), 'use_sim_time': use_sim_time}
+    # doc = xacro.parse(open(xacro_file))
+    # xacro.process_doc(doc)
+    # robot_description_xml = doc.toxml()
+
+    robot_description_config= Command(['xacro ', xacro_file,
+                                       ' use_simulation:=', use_simulation])
+    robot_description_xml = ParameterValue(robot_description_config, value_type=str)
+
+    params = {'robot_description': robot_description_xml, 'use_sim_time': use_sim_time}
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -45,6 +57,7 @@ def generate_launch_description():
 
     # add the necessary declared launch arguments to the launch description
     ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_use_simulation_cmd)
 
     # Add the nodes to the launch description
     ld.add_action(robot_state_publisher_node)
