ff1f1c07af56862f06e4f2965cfbe9827a1c0ab0
samuko-things <samuel.c.agba@gmail.com>
2024-10-07 23:55:59 +0100

    update mobo_bot_base  launch file

--- mobo_bot_base/launch/robot.launch.py
+++ mobo_bot_base/launch/robot.launch.py
@@ -22,8 +22,8 @@ def generate_launch_description():
 
     declare_use_ekf_cmd = DeclareLaunchArgument(
       name='use_ekf',
-      default_value='True',
-      # default_value='False',
+      # default_value='True',
+      default_value='False',
       description='fuse odometry and imu data if true')
 
     declare_odom_topic_cmd = DeclareLaunchArgument(
